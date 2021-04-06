2de2268c7a0370d039063758e1029b1f319b3628
Hans <hans.schneebeli@gmail.com>
2021-04-06 17:14:58 -0300

    Added read_probe to test external RAM access

--- 22-ExternalRAM/main.c
+++ 22-ExternalRAM/main.c
@@ -1,3 +1,4 @@
+
 /**
  * @file     main.c
  * @brief    Blink LEDs using counting delays and CMSIS (Heavy use of macros)
@@ -10,6 +11,8 @@
  *
  *
  ******************************************************************************/
+
+#include <stdio.h>
 
 #include "stm32f746xx.h"
 #include "system_stm32f746.h"
@@ -102,24 +105,89 @@ PLL_Configuration  pllsaiconfig  = {
  * @param[in] read_address The address to try reading a byte from
  * @return Returns true if no bus fault occurred reading from read_address, or false if a bus fault occurred.
  */
-bool read_probe (volatile const char *read_address)
-{
-    bool address_readable = true;
 
-    /* Clear any existing indication of a bus fault - BFARV is write one to clear */
-    HWREG (NVIC_FAULT_STAT) |= NVIC_FAULT_STAT_BFARV;
 
-    HWREG (NVIC_CFG_CTRL) |= NVIC_CFG_CTRL_BFHFNMIGN;
-    asm volatile ("  CPSID f;");
-    *read_address;
-    if ((HWREG (NVIC_FAULT_STAT) & NVIC_FAULT_STAT_BFARV) != 0)
-    {
-        address_readable = false;
+/*
+ * From
+ * System control block (SCB)
+ * The System Control Block (SCB) provides system implementation information,
+ * and system control. This includes configuration, control, and reporting
+ * of the system exceptions. The system control block registers are:
+ * ...
+ * 0xE000ED14 CCR
+ * ...
+ * 0xE000ED28 CFSR (Word access)
+ * 0xE000ED29 BFSR (Byte access)
+ * ...
+ *
+ * Configurable fault status register (CFSR)
+ * The CFSR indicates the cause of a MemManage fault, BusFault, or UsageFault.
+ * It has three fields: UFSR, BFSR, MMFSR
+ *
+ * Flag BFARVALID in BFSR
+ * * BusFault Address register (BFAR) valid flag:
+ * 0: Value in BFAR is not a valid fault address.
+ * 1: BFAR holds a valid fault address.
+ * The processor sets this bit to 1 after a BusFault where the address is known.
+ *  Other faults can set this bit to 0, such as a MemManage fault occurring
+ * later. If a BusFault occurs and is escalated to a hard fault because of
+ *  priority, the hard fault handler must set this bit to 0. This prevents
+ *  problems if returning to a stacked active BusFault handler whose BFAR value
+ *  has been overwritten.
+
+ * Configuration and control register (CCR)
+ * The CCR controls entry to Thread mode and enables:
+ *
+ * BFHFNMIGN Flag
+ * Enables handlers with priority -1 or -2 to ignore data BusFaults
+ * caused by load and store instructions. This applies to the hard
+ * fault, NMI, and FAULTMASK escalated handlers:
+ * 0:Data bus faults caused by load and store instructions
+ * cause a lock-up.
+ * 1: Handlers running at priority -1 and -2 ignore data bus
+ * faults caused by load and store instructions.
+ * Set this bit to 1 only when the handler and its data are in
+ * absolutely safe memory. The normal use of this bit is to probe
+ * system devices and bridges to detect control path problems
+ * and fix them
+ */
+#define NVIC_FAULT_STAT             (*(uint32_t) 0xE000ED28)
+// Bus Fault Address Register Valid
+#define NVIC_FAULT_STAT_BFARV       0x00008000
+
+#define NVIC_CFG_CTRL               (*(uint32_t) 0xE000ED14)
+#define  NVIC_CFG_CTRL_BFHFNMIGN    0x00000100
+
+
+int read_probe (volatile const char *address) {
+int status;
+volatile char ch;
+
+    /*
+     * Clear an existing indication of a bus fault. Just in case.
+     * Writing 1 to BFARVALID bit in CFSR clears it
+     */
+    SCB->CFSR |= SCB_CFSR_BFARVALID_Msk;
+
+    /*
+     * Ignores BusFaults by load and store instructions
+     * by setting BFHFNMIGN to 1
+     */
+    SCB->CCR |= SCB_CCR_BFHFNMIGN_Msk;
+
+    status = 1;
+    __disable_fault_irq(); /* CPSID f */
+
+    ch = *address;  /* <=== Memory access */
+    if( (SCB->CFSR&SCB_CFSR_BFARVALID_Msk) != 0 ) {
+        status = 0;
     }
-    asm volatile ("  CPSIE f;");
-    HWREG (NVIC_CFG_CTRL) &= ~NVIC_CFG_CTRL_BFHFNMIGN;
+    __enable_fault_irq(); /* CPSIE f */
 
-    return address_readable;
+    /* Reenable BusFaults */
+    SCB->CCR &= ~SCB_CCR_BFHFNMIGN_Msk;
+
+    return status;
 }
 
 
